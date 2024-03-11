b02831c0c3885925bec4cd93ade5ec116e28956e
Jazz <webmaster@codej.ru>
2024-03-11 07:05:16 +0500

    Show labeled drafts

--- sandbox/static/js/drafts/labeled.js
+++ sandbox/static/js/drafts/labeled.js
@@ -5,4 +5,29 @@ $(function() {
   formatFooter(dt);
   $('body').on('click', '.close-top-flashed', closeTopFlashed);
   showLabeledDrafts('/api/labels', page, label, dt);
+  if (window.localStorage.getItem('token')) {
+    $('body').on('click', '.entity-alias a', function(event) {
+      event.preventDefault();
+    });
+    $('body').on('click', '.page-link',{label: label}, function(event) {
+      event.preventDefault();
+      let th = $(this).parent();
+      if (!th.hasClass('active')) {
+        window.location.assign(
+          '/drafts/t/' + event.data.label + '?page=' + $(this).text().trim());
+      }
+    });
+    $('body')
+    .on('click', '#next-link', {page: page, label: label}, function(event) {
+      event.preventDefault();
+      let p = parseInt(event.data.page.trim()) + 1;
+      window.location.assign('/drafts/t/' + event.data.label + '?page=' + p);
+    });
+    $('body')
+    .on('click', '#prev-link', {page: page, label: label}, function(event) {
+      event.preventDefault();
+      let p = parseInt(event.data.page.trim()) - 1;
+      window.location.assign('/drafts/t/' + event.data.label + '?page=' + p);
+    });
+  }
 });
