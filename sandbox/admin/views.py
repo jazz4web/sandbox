d5a1180a51cb57bafff46c7eeb70f5345e15a090
Jazz <webmaster@codej.ru>
2024-03-22 07:10:19 +0500

    Fix an issue

--- sandbox/admin/views.py
+++ sandbox/admin/views.py
@@ -4,31 +4,26 @@ from starlette.exceptions import HTTPException
 from starlette.responses import FileResponse, PlainTextResponse
 
 from ..auth.attri import get_group, groups
-from ..common.pg import get_conn
+from ..auth.cu import getcu
 
 
 async def show_log(request):
     l = request.path_params.get('log')
     if l not in ('access.log', 'previous.log'):
         raise HTTPException(404, detail='Такой страницы у нас нет.')
-    cu = int((await request.app.rc.hgetall(
-        request.session.get('_uid', 'empty'))).get('id', '0'))
+    cu = await getcu(request)
     if cu:
-        conn = await get_conn(request.app.config)
-        cu = await conn.fetchrow(
-            'SELECT id, permissions FROM users WHERE id = $1', cu)
-        if cu:
-            group = await get_group(cu.get('permissions'))
-            if group in (groups.keeper, groups.root):
-                if l == 'access.log':
-                    l = f'/var/log/nginx/{l}'
-                else:
-                    l = '/var/log/nginx/access.log.1'
-                if os.path.exists(l):
-                    response = FileResponse(l)
-                else:
-                    a = 'Файл не существует.\n'
-                    m = 'Убедитесь, что вы используете Nginx.'
-                    response = PlainTextResponse(a + m)
-                return response
+        group = await get_group(cu.get('permissions'))
+        if group in (groups.keeper, groups.root):
+            if l == 'access.log':
+                l = f'/var/log/nginx/{l}'
+            else:
+                l = '/var/log/nginx/access.log.1'
+            if os.path.exists(l):
+                response = FileResponse(l)
+            else:
+                a = 'Файл не существует.\n'
+                m = 'Убедитесь, что вы используете Nginx.'
+                response = PlainTextResponse(a + m)
+            return response
     raise HTTPException(404, detail='Такой страницы у нас нет.')
